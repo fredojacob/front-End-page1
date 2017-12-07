1832d8813a398cd6a7b3e97aed4e9b3bd0397bac
Fredo <31228779+fredojacob@users.noreply.github.com>
2017-12-07 10:32:55 -0600

    DPF
    DPFDPF

---
+++ DPFPP/index.php
@@ -0,0 +1,305 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PDF-DeveloperProsefionalFinder</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Chelsea+Market" rel="stylesheet">  
+    <!-- Custom CSS -->
+    <!--Animate-->
+    <link rel="stylesheet" href="animate.min.css">
+    <link rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css">
+  <!-- or -->
+  <link rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  <!--shake-->
+  
+  
+
+  <!--/Animate-->
+    <link href="css/stylish-portfolio.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <!-- Navigation -->
+    <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle">
+      <i class="fa fa-bars"></i>
+    </a>
+    <nav id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle">
+          <i class="fa fa-times"></i>
+        </a>
+        <li class="sidebar-brand">
+          <a class="js-scroll-trigger" href="#top"> < PDF ></a>
+        </li>
+        <li>
+      <!--    <a class="js-scroll-trigger" href="#top">Home</a>
+        </li> -->
+       <!-- <li>
+          <a class="js-scroll-trigger" href="#about">About</a>
+        </li> -->
+        <li>
+          <a class="js-scroll-trigger" href="#services">Servicios</a>
+        </li>
+       <!-- <li>
+          <a class="js-scroll-trigger" href="#portfolio">Portfolio</a>
+        </li> -->
+        <li>
+          <a class="js-scroll-trigger" href="./Policy.php">Politicas</a>
+        </li>
+        <li>
+          <a class="js-scroll-trigger" href="./Terms.php">Terminos de Servicio</a>
+        </li>
+        <li>
+          <a class="js-scroll-trigger" href="#contact" onclick=$( "#menu-close").click();>Contacto</a>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Header -->
+    <header class="header" id="top">
+      <div class="text-vertical-center dpf">
+      <img class="img-portfolio img-fluid " src="img/pdf.png">
+      <!--  <h1>Developer Professional Finder</h1> -->
+      <br></br>
+      <br></br>
+      
+        <h3 class="font1 animated zoomInDown">La Base de Datos de los Desarrolladores Profesionales</h3>
+        <h3 class="font1 animated fadeInLeft">para Sistemas Informáticos y Soluciones Digitales.</h3>
+        <br>
+        <a href="#about" class="btn btn-dark btn-lg js-scroll-trigger">¡Ingresa aquí!</a>
+      </div>
+    </header>
+    <!-- About -->
+    <section id="about" class="about">
+      <div class="container text-center">
+        <h2>Diseñadores, Desarrolladores, Administradores, encuentra las mejores opciones</h2>
+        <h2>Recursos Humanos Validados para mayor productividad.</h2>
+
+        <p class="lead">Compañías Internacionales reclutan desde aquí. <a target="_blank" href="http://join.deathtothestockphoto.com/">Descubra cómo lo hacen</a>.</p>
+      </div>
+      <!-- /.container -->
+    </section>
+
+    <!-- Services -->
+    <section id="services" class="services bg-primary text-white">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-lg-10 mx-auto">
+            <h2>Nuestros Servicios</h2>
+            <hr class="small">
+            <div class="row">
+              <div class="col-md-3 col-sm-6">
+                <div class="service-item">
+                  <span class="fa-stack fa-4x">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-vcard-o fa-stack-1x text-primary"></i>
+                  </span>
+                  <h4>
+                    <strong>Perfiles</strong>
+                  </h4>
+                  <p>Los Profesionales de la Informática, ingresan sus destrezas y habilidades.</p>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6">
+                <div class="service-item">
+                  <span class="fa-stack fa-4x">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-code fa-stack-1x text-primary"></i>
+                  </span>
+                  <h4>
+                    <strong>Tecnologías</strong>
+                  </h4>
+                  <p>Esta Sección describe el expertice de los candidatos, es totalmente sin costo.</p>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6">
+                <div class="service-item">
+                  <span class="fa-stack fa-4x">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-tasks fa-stack-1x text-primary"></i>
+                  </span>
+                  <h4>
+                    <strong>Validaciones</strong>
+                  </h4>
+                  <p>Pruebas técnicas elaboradas por Desarrolladores en nuestra Comunidad.</p>
+                </div>
+              </div>
+              <div class="col-md-3 col-sm-6">
+                <div class="service-item">
+                  <span class="fa-stack fa-4x">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-handshake-o fa-stack-1x text-primary"></i>
+                  </span>
+                  <h4>
+                    <strong>Empleadores</strong>
+                  </h4>
+                  <p>Sección exclusiva para Empresas que reclutan profesionales.</p>
+                  <!--<a href="#" class="btn btn-light">Seguir</a>-->
+                </div>
+              </div>
+            </div>
+            <!-- /.row (nested) -->
+          </div>
+          <!-- /.col-lg-10 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+
+    <!-- Callout -->
+    <aside class="callout parallax_as">
+      <div class="text-vertical-center">
+       
+      </div>
+    </aside>
+
+    <!-- Portfolio -->
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <section id="portfolio" class="portfolio">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-10 mx-auto text-center">
+            <h2>Herramientas de la Implementación</h2>
+            <hr class="small">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="portfolio-item">
+                  <a href="#">
+                    <img class="img-portfolio img-fluid" src="img/reactlogo.jpg">
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="portfolio-item">
+                  <a href="#">
+                    <img class="img-portfolio img-fluid" src="img/phprest.jpg">
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="portfolio-item">
+                  <a href="#">
+                    <img class="img-portfolio img-fluid" src="img/basesdedatos.jpg">
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-6 margin">
+                <div class="portfolio-item">
+                  <a href="#">
+                    <img class="img-portfolio img-fluid" src="img/facebook.jpg">
+                  </a>
+                </div>
+              </div>
+            <div class="text-vertical-center col-md-12 margin">
+              <h3>Base de Datos - Redes y Servidores – Inteligencia Artificial</h3> 
+              <h4>Robótica - Sistemas operativos - Soporte Técnico</h4> 
+              <h4>Videojuegos - Apps</h4>
+                <br>
+              <h2 class="font"><strong>¡LEARN ONES, WRITE ENYWHERE!</strong></h2>
+            </div>
+              </div>
+            <!-- /.row (nested) -->
+          <!--  <a href="#" class="btn btn-dark">Ver Más y toma acción</a> -->
+          </div>
+          <!-- /.col-lg-10 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+  </div>
+    <!-- Call to Action -->
+    <div class="parallax"></div>
+    <aside class="call-to-action bg-primary text-white">
+      <div class="container text-center">
+      
+        <h3>Toma acción y se parte del mundo del Desarrollo Informático.</h3>
+        <a href="#" class="btn btn-lg btn-light">Ingresa</a>
+       <!-- <a href="#" class="btn btn-lg btn-dark">Vea Mas!</a> -->
+      </div>
+    </aside>
+    <div class="parallax"></div>
+    <!-- Map -->
+    <section id="contact" class="map">
+    <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ-RkyuxmiiYURMkZXlcm_pjs&key=AIzaSyAAn52rYVs2tM7R4B9HaTTXtuJSW6rdIT8" allowfullscreen></iframe>
+  
+      <br/>
+      <small>
+        <a href="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ-RkyuxmiiYURMkZXlcm_pjs&key=AIzaSyAAn52rYVs2tM7R4B9HaTTXtuJSW6rdIT8"></a>
+      </small>
+    </section> 
+
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-10 mx-auto text-center">
+            <h4>
+              <strong>DPF Company</strong>
+            </h4>
+            <p>Laboratorio Z13 Ciudad de Guatemala
+              <br>Guatemala, Centroamérica</p>
+            <ul class="list-unstyled">
+              <li>
+                <i class="fa fa-phone fa-fw"></i>
+                (502) 1234-5678</li>
+              <li>
+                <i class="fa fa-envelope-o fa-fw"></i>
+                <a href="mailto:name@example.com">admin@dpf.com</a>
+              </li>
+            </ul>
+            <br>
+            <ul class="list-inline">
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-facebook fa-fw fa-3x"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+              <a href="#">
+                  <i class="fa fa-youtube fa-fw fa-3x"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+              <a href="#">
+                  <i class="fa fa-twitter fa-fw fa-3x"></i>
+                </a>
+              </li>
+            </ul>
+            <hr class="small">
+            <p class="text-muted">Copyright &copy; www.dpf.com 2017</p>
+          </div>
+        </div>
+      </div>
+      <a id="to-top" href="#top" class="btn btn-dark btn-lg js-scroll-trigger">
+        <i class="fa fa-chevron-up fa-fw fa-1x"></i>
+      </a>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/stylish-portfolio.js"></script>
+
+  </body>
+
+</html>
